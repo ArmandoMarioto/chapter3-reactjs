f0928073aa9c95c9aba212b6a7e00cead93551cb
ArmandoMarioto <armando_marioto100@hotmail.com>
2022-04-05 10:49:02 -0300

    Salvando usuaruio no banco

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github";
 
+import { query as q } from "faunadb"
+
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
   providers: [
     GitHubProvider({
@@ -8,4 +12,30 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  jwt:{
+    secret: process.env.JWT_SECRET,
+  },
+
+  callbacks: {
+    async signIn(user, account, profile) {
+      
+      const email = user.user.email;
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection("users"),
+            {data: {email}}
+          )
+        )
+        return true;
+      } catch (error) {
+
+        return false;
+      }
+     
+    } 
+  }
+
+
+
 })
