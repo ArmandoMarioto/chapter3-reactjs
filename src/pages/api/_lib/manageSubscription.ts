b233e75b93b66346de0073481cce390ff0a0add6
ArmandoMarioto <armando_marioto100@hotmail.com>
2022-07-12 20:38:01 -0300

    Ouvindo mais  eventos

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,6 +5,7 @@ import { stripe } from "../../../services/stripe";
 export async function saveSubscriptions(
     subscriptionID: string,
     customerID: string,
+    createAction = false,
 )
 {
     const userRef = await fauna.query(
@@ -27,13 +28,30 @@ export async function saveSubscriptions(
         status: subscription.status,
         price: subscription.items.data[0].price.id
     }
-    console.log(subscriptionData);
-    await fauna.query(
-        q.Create(
-            q.Collection('subscriptions'),
-            { data : subscriptionData }
 
+    if(createAction){
+        await fauna.query(
+            q.Create(
+                q.Collection('subscriptions'),
+                { data : subscriptionData }
+    
+            )
         )
-    )
+    } else {
+        await fauna.query(
+            q.Replace(
+                q.Select(
+                    "ref",
+                    q.Get(
+                        q.Match(
+                            q.Index('subscription_by_id'),
+                            subscription.id
+                    )
+                    )
+                ),
+                { data : subscriptionData }
+            )
+        )
+    }
     
 }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -17,7 +17,12 @@ export const config = {
     bodyParser: false,
   },
 };
-const RelevantEvents = new Set(["checkout.session.completed"]);
+const RelevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.created",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
@@ -41,12 +46,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (RelevantEvents.has(type)) {
       try {
         switch (type) {
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription;
+            await saveSubscriptions(subscription.id, subscription.customer.toString(),
+              false);
+            break;
           case "checkout.session.completed":
             const checkoutSession = event.data
               .object as Stripe.Checkout.Session;
             await saveSubscriptions(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             );
             break;
           default:
